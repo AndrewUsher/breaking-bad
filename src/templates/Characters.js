3c4430c3a11ee5f9ef3c6d12ea7ec4fbd0051d71
Andrew Usher <andrewusher00@gmail.com>
2020-04-22 17:14:06 -0500

    feat: add information to characters page

--- src/templates/Characters.js
+++ src/templates/Characters.js
@@ -1,13 +1,20 @@
 import React, { Fragment } from 'react'
+import styled from 'styled-components'
+
+const CharacterImage = styled.img`
+  max-height: 200px;
+`
 
 const Deaths = ({ pageContext: { characters } }) => {
   console.log(characters)
   return (
     <Fragment>
       <h1>Characters</h1>
-      {characters.map(({ char_id: characterId, img }) => (
+      {characters.map(({ char_id: characterId, img, name, occupation }) => (
         <article key={characterId}>
-          <img src={img} />
+          <h2>{name}</h2>
+          <CharacterImage src={img} loading="lazy" />
+          <h3>Occupations: {occupation.join(', ')}</h3>
         </article>
       ))}
 
